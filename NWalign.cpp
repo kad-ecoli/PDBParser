645bde436d82f44811d771e55efb063a6511dfd5
zcx <zcx@umich.edu>
2017-05-14 22:32:49 -0400

    NWalign can print max seqID only

--- NWalign.cpp
+++ NWalign.cpp
@@ -11,6 +11,10 @@ const char* docstring=""
 "\n"
 "    NWalign input1 input2 option+10 (only print sequence identity)\n"
 "    NWalign input1 input2 option+20 (only print fasta sequence)\n"
+"    NWalign input1 input2 option+30 (for each input1 entry, only print\n"
+"                                     input2 entry with max identity)\n"
+"\n"
+//"    NWalign input1 input2 option+100 (glocal-both alignment)\n"
 ;
 
 #include <iostream>
@@ -25,6 +29,7 @@ int main(int argc, char **argv)
 {
     /* parse commad line argument */
     int input_mode=0; // align two fasta files
+    int glocal=0;     // global or glocal-both alignment
     int seqID_only=0; // do not just print seqID
     if(argc<3)
     {
@@ -34,7 +39,20 @@ int main(int argc, char **argv)
     if (argc>3)
     {
         input_mode=atoi(argv[3]);
-        if (input_mode>=20)
+        // alignment algorithm
+        if (input_mode>=100)
+        {
+            glocal=1;
+            input_mode-=100;
+        }
+
+        // output format
+        if (input_mode>=30)
+        {
+            seqID_only=3;
+            input_mode-=30;
+        }
+        else if (input_mode>=20)
         {
             seqID_only=2;
             input_mode-=20;
@@ -107,46 +125,49 @@ int main(int argc, char **argv)
 
                 string aln1,aln2;
                 int aln_score=NWalign(seq1,seq2,seq2int1,seq2int2,aln1,aln2,
-                    BLOSUM62,gapopen_blosum62,gapext_blosum62);
+                    BLOSUM62,gapopen_blosum62,gapext_blosum62,glocal);
 
                 string aln_str; // colon for identical sequence
                 string pos_str; // last digit for position index
                 int iden_len,aln_len;  // num of identical/aligned positions
                 get_seqID(aln1,aln2,aln_str,pos_str,iden_len,aln_len);
 
-                if (seqID_only==2)
+                if (seqID_only==2) // fasta alignment only
                 {
                     cout<<'>'<<name1<<endl<<aln1<<endl;
                     cout<<'>'<<name2<<endl<<aln2<<endl;
-                    continue;
+                    if (q<seq_num1-1 || s<seq_num2-1) cout<<"$$$$\n"<<endl;
                 }
-                else if (seqID_only==1)
+                else if (seqID_only==1) // seqID only
                 {
                     cout<<name1<<'\t'<<name2<<'\t';
-                    cout<<setprecision(4)<<float(iden_len)/len1<<'\t';
-                    cout<<setprecision(4)<<float(iden_len)/len2<<endl;
-                    continue;
+                    cout<<setiosflags(ios::fixed)<<setprecision(4);
+                    cout<<float(iden_len)/len1<<'\t';
+                    cout<<setiosflags(ios::fixed)<<setprecision(4);
+                    cout<<float(iden_len)/len2<<endl;
                 }
+                else // full output
+                {
+                    cout<<"Length of sequence 1:"<<setw(5)<<len1;
+                    cout<<" ->"<<name1<<endl;
+                    cout<<"Length of sequence 2:"<<setw(5)<<len2;
+                    cout<<" ->"<<name2<<endl;
 
-                cout<<"Length of sequence 1:"<<setw(5)<<len1;
-                cout<<" ->"<<name1<<endl;
-                cout<<"Length of sequence 2:"<<setw(5)<<len2;
-                cout<<" ->"<<name2<<endl;
+                    cout<<"Alignment score:"<<aln_score<<endl;
+                    cout<<"Aligned length:"<<setw(5)<<aln_len<<endl;
+                    cout<<"Identical length:"<<setw(5)<<iden_len<<endl;
+                    cout<<"Sequence identity:"<<setiosflags(ios::fixed);
+                    cout<<setprecision(3)<<setw(9)<<float(iden_len)/len2;
+                    cout<<" (="<<setw(4)<<iden_len<<'/'<<setw(4)<<len2;
+                    cout<<")\n\n";
 
-                cout<<"Alignment score:"<<aln_score<<endl;
-                cout<<"Aligned length:"<<setw(5)<<aln_len<<endl;
-                cout<<"Identical length:"<<setw(5)<<iden_len<<endl;
-                cout<<"Sequence identity:"<<setiosflags(ios::fixed);
-                cout<<setprecision(3)<<setw(9)<<float(iden_len)/len2<<" (=";
-                cout<<setw(4)<<iden_len<<'/'<<setw(4)<<len2<<')'<<endl;
-                cout<<endl;
+                    cout<<aln1<<endl;
+                    cout<<aln_str<<endl;
+                    cout<<aln2<<endl;
+                    cout<<pos_str<<endl;
 
-                cout<<aln1<<endl;
-                cout<<aln_str<<endl;
-                cout<<aln2<<endl;
-                cout<<pos_str<<endl;
-
-                if (q<seq_num1-2 || s<seq_num1-1) cout<<"$$$$\n"<<endl;
+                    if (q<seq_num1-2 || s<seq_num1-1) cout<<"$$$$\n"<<endl;
+                }
             }
         }
         return 0;
@@ -159,6 +180,9 @@ int main(int argc, char **argv)
         string seq1=seq_list1[q];
         vector<int> seq2int1=seq2int_list1[q];
         int len1=seq1.length();
+        int max_iden_len=0; // identical positions with max seqID seq2
+        string max_seqID_name2="";
+        int max_seqID_len2=0;
 
         for (int s=0;s<seq_num2;s++)
         {
@@ -169,46 +193,68 @@ int main(int argc, char **argv)
 
             string aln1,aln2;
             int aln_score=NWalign(seq1,seq2, seq2int1,seq2int2, aln1,aln2,
-                BLOSUM62,gapopen_blosum62,gapext_blosum62);
+                BLOSUM62,gapopen_blosum62,gapext_blosum62,glocal);
 
             string aln_str; // colon for identical sequence
             string pos_str; // last digit for position index
             int iden_len,aln_len;  // num of identical/aligned positions
             get_seqID(aln1,aln2,aln_str,pos_str,iden_len,aln_len);
 
-            if (seqID_only==2)
+            if (seqID_only==3) // max seqID only
+            {
+                if (max_iden_len<=iden_len)
+                {
+                    max_iden_len=iden_len;
+                    max_seqID_name2=name2;
+                    max_seqID_len2=len2;
+                }
+            }
+            else if (seqID_only==2) // fasta alignment only
             {
                 cout<<'>'<<name1<<endl<<aln1<<endl;
                 cout<<'>'<<name2<<endl<<aln2<<endl;
-                continue;
+                if (q<seq_num1-1 || s<seq_num2-1) cout<<"$$$$\n"<<endl;
             }
-            else if (seqID_only==1)
+            else if (seqID_only==1) // seqID only
             {
                 cout<<name1<<'\t'<<name2<<'\t';
-                cout<<setprecision(4)<<float(iden_len)/len1<<'\t';
-                cout<<setprecision(4)<<float(iden_len)/len2<<endl;
-                continue;
+                cout<<setiosflags(ios::fixed)<<setprecision(4);
+                cout<<float(iden_len)/len1<<'\t';
+                cout<<setiosflags(ios::fixed)<<setprecision(4);
+                cout<<float(iden_len)/len2<<endl;
+                if (seq_num1>1 && s==seq_num2-1) cout<<"$$$$\n";
+            }
+            else // full output
+            {
+                cout<<"Length of sequence 1:"<<setw(5)<<len1;
+                cout<<" ->"<<name1<<endl;
+                cout<<"Length of sequence 2:"<<setw(5)<<len2;
+                cout<<" ->"<<name2<<endl;
+
+                cout<<"Alignment score:"<<setw(5)<<aln_score<<endl;
+                cout<<"Aligned length:"<<setw(5)<<aln_len<<endl;
+                cout<<"Identical length:"<<setw(5)<<iden_len<<endl;
+                cout<<"Sequence identity:"<<setiosflags(ios::fixed);
+                cout<<setprecision(3)<<setw(9)<<float(iden_len)/len2;
+                cout<<" (="<<setw(4)<<iden_len<<'/'<<setw(4)<<len2;                
+                cout<<")\n\n";
+
+                cout<<aln1<<endl;
+                cout<<aln_str<<endl;
+                cout<<aln2<<endl;
+                cout<<pos_str<<endl;
+                if (q<seq_num1-1 || s<seq_num2-1) cout<<"$$$$\n"<<endl;
             }
 
-            cout<<"Length of sequence 1:"<<setw(5)<<len1;
-            cout<<" ->"<<name1<<endl;
-            cout<<"Length of sequence 2:"<<setw(5)<<len2;
-            cout<<" ->"<<name2<<endl;
-
-            cout<<"Alignment score:"<<setw(5)<<aln_score<<endl;
-            cout<<"Aligned length:"<<setw(5)<<aln_len<<endl;
-            cout<<"Identical length:"<<setw(5)<<iden_len<<endl;
-            cout<<"Sequence identity:"<<setiosflags(ios::fixed);
-            cout<<setprecision(3)<<setw(9)<<float(iden_len)/len2<<" (=";
-            cout<<setw(4)<<iden_len<<'/'<<setw(4)<<len2<<')'<<endl;
-            cout<<endl;
-
-            cout<<aln1<<endl;
-            cout<<aln_str<<endl;
-            cout<<aln2<<endl;
-            cout<<pos_str<<endl;
-
-            if (q<seq_num1-1 || s<seq_num2-1) cout<<"$$$$\n"<<endl;
+        }
+        
+        if (seqID_only==3)
+        {
+            cout<<name1<<'\t'<<max_seqID_name2<<'\t';
+            cout<<setiosflags(ios::fixed)<<setprecision(4);
+            cout<<float(max_iden_len)/len1<<'\t';
+            cout<<setiosflags(ios::fixed)<<setprecision(4);
+            cout<<float(max_iden_len)/max_seqID_len2<<endl;
         }
     }
     return 0;
